d8a5e3aba5ae82f474fa1fe266b428d2833526b9
Tadachi274 <ryoryoryoryoji@icloud.com>
2025-07-08 17:47:42 +0900

    replaylist

--- infra/db/migrations/20250617104155_sample.up.sql
+++ infra/db/migrations/20250617104155_sample.up.sql
@@ -1,4 +1,8 @@
-CREATE TABLE IF NOT EXISTS sample (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE spotify_tokens (
+    access_token TEXT NOT NULL,
+    token_type VARCHAR(50) NOT NULL,
+    expires_in INTEGER NOT NULL,
+    refresh_token TEXT NOT NULL,
+    scope TEXT NOT NULL,
+    expires_at BIGINT NOT NULL
 );
