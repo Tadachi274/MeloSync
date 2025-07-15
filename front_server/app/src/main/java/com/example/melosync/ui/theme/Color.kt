cd6e2ea1ca3e5219f46ca8b2f926cb3e3464d217
somacoro <coron.214.soma@gmail.com>
2025-07-16 02:47:35 +0900

    初期の感情位置固定点追加、プレイリス変更用ボタンなど追加、SpotifyAPI用ライブラリ追加

--- front_server/app/src/main/java/com/example/melosync/ui/theme/Color.kt
+++ front_server/app/src/main/java/com/example/melosync/ui/theme/Color.kt
@@ -7,14 +7,19 @@ val Purple80 = Color(0xFFE5E3F6)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
+val Purple40 = Color(0xFF7B4E80)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val PastPoint = Color(0x6B939393)
+val CurrentPoint = Color(0xFFFFFFFF)
+val Shadow = Color(0x5EB9B9B9)
+
 val AppPurple = Color(0xFF432BFF)
 val AppCyan = Color(0xFF86D8D5)
+val AppPurple2 = Color(0xFF7B4E80)
 val AppPink = Color(0xFFE8A9BE)
-val AppBackground = Color(0xFFF4F1FF)
+val AppBackground = Color(0xFFEAE6F5)
 val AppDarkBackground = Color(0xFFD5D5D5)
 
 val AppBackgroundBrush = Brush.verticalGradient(
