140fbcf90ec5b8c2e350f0d48de6b6f3da036403
somacoro <coron.214.soma@gmail.com>
2025-07-29 08:48:07 +0900

    loadQueue導入

--- front_server/app/src/main/java/com/example/melosync/ApiClient.kt
+++ front_server/app/src/main/java/com/example/melosync/ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
     val retrofit = Retrofit.Builder()
-        .baseUrl("http://10.42.15.227:5000/") // 如果用模擬器，改成 http://10.0.2.2:5000
+        .baseUrl("http://172.20.10.14:5000/") // 如果用模擬器，改成 http://10.0.2.2:5000
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
