634a341bc12cd6ad5a440b16d982b9ea718096e9
somacoro <coron.214.soma@gmail.com>
2025-07-29 08:48:07 +0900

    loadQueue導入

--- front_server/app/src/main/java/com/example/melosync/ui/auth/NetworkModule.kt
+++ front_server/app/src/main/java/com/example/melosync/ui/auth/NetworkModule.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkModule {
     //private const val BASE_URL = "https://d9b860c73694.ngrok-free.app"
-    private const val BASE_URL = "http://10.0.2.2:8000"
+    private const val BASE_URL = "http://172.20.10.14:8000"
     val authApi: AuthApi = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
