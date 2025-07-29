f6014dc35244e4b5f4493c8ec5664a5d54d3b884
Tadachi274 <ryoryoryoryoji@icloud.com>
2025-07-29 11:30:31 +0900

    ボタンに影をつける、更新ボタンとログインボタンにサクセスメッセージ追加

--- front_server/app/src/main/java/com/example/melosync/ui/auth/AuthButton.kt
+++ front_server/app/src/main/java/com/example/melosync/ui/auth/AuthButton.kt
@@ -1,13 +1,29 @@
 // ui/auth/AuthButton.kt
 package com.example.melosync.ui.auth
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import com.example.melosync.ui.theme.AppBackground
+import com.example.melosync.ui.theme.AppPurple2
 
 @Composable
 fun AuthButton(onClick: () -> Unit) {
-    Button(onClick = onClick) {
+    Button(
+        colors = ButtonDefaults.buttonColors(
+            containerColor = AppPurple2,
+            contentColor = AppBackground
+        ),
+        shape = RoundedCornerShape(50), // 丸み
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 6.dp,
+            pressedElevation = 8.dp,
+            disabledElevation = 0.dp
+        ),
+        onClick = onClick) {
         Text("ログイン")
     }
 }
