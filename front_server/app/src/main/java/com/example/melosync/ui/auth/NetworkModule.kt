d03b7066eb64de37f93cf91d337d7d26d400d74b
Tadachi274 <ryoryoryoryoji@icloud.com>
2025-07-29 11:30:31 +0900

    ボタンに影をつける、更新ボタンとログインボタンにサクセスメッセージ追加

--- front_server/app/src/main/java/com/example/melosync/ui/auth/NetworkModule.kt
+++ front_server/app/src/main/java/com/example/melosync/ui/auth/NetworkModule.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkModule {
-    //private const val BASE_URL = "https://d9b860c73694.ngrok-free.app"
+    //private const val BASE_URL = "https://bd1a97e38112.ngrok-free.app"
     private const val BASE_URL = "http://10.0.2.2:8000"
     val authApi: AuthApi = Retrofit.Builder()
         .baseUrl(BASE_URL)
